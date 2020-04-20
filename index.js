19b5fdf210271158f7c4446f35cf6b1196a66c3d
Linus Unnebäck <linus@folkdatorn.se>
2020-04-20 08:15:00 +0100

    Warn when using insecure random number generator

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const base64Decode = require('fast-base64-decode')
 class TypeMismatchError extends Error {}
 class QuotaExceededError extends Error {}
 
+let warned = false
+function insecureRandomValues (array) {
+  if (!warned) {
+    console.warn('Using an insecure random number generator, this should only happen when running with a debugger without support for crypto.getRandomValues')
+    warned = true
+  }
+
+  for (let i = 0, r; i < array.length; i++) {
+    if ((i & 0x03) === 0) r = Math.random() * 0x100000000
+    array[i] = (r >>> ((i & 0x03) << 3)) & 0xff
+  }
+
+  return array
+}
+
 /**
  * @param {Int8Array|Uint8Array|Int16Array|Uint16Array|Int32Array|Uint32Array|Uint8ClampedArray} array
  */
@@ -15,17 +30,12 @@ function getRandomValues (array) {
   if (array.byteLength > 65536) {
     throw new QuotaExceededError('Can only request a maximum of 65536 bytes')
   }
-  
+
   // Calling RNGetRandomValues.getRandomBase64 in debug mode leads to the error
   // "Calling synchronous methods on native modules is not supported in Chrome".
   // So in that specific case we fall back to just using Math.random.
   if (__DEV__) {
-    for (var i = 0, r; i < array.length; i++) {
-      if ((i & 0x03) === 0) r = Math.random() * 0x100000000;
-      array[i] = (r >>> ((i & 0x03) << 3)) & 0xff;
-    }
-
-    return array;
+    return insecureRandomValues(array)
   }
 
   base64Decode(RNGetRandomValues.getRandomBase64(array.byteLength), new Uint8Array(array.buffer, array.byteOffset, array.byteLength))
