5eb0696eca37300ff42c9eb200217add17bc80a8
Linus Unnebäck <linus@folkdatorn.se>
2021-04-12 14:01:23 +0200

    🌹 Move getRandomBase64 back into main file

--- getRandomBase64.js
+++
@@ -1,7 +0,0 @@
-const { NativeModules } = require('react-native')
-
-if (NativeModules.RNGetRandomValues) {
-  module.exports = NativeModules.RNGetRandomValues.getRandomBase64;
-} else if (NativeModules.ExpoRandom) {
-  module.exports = NativeModules.ExpoRandom.getRandomBase64String
-}
