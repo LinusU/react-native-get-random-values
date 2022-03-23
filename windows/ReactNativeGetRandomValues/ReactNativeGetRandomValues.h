29985e3e763e331f57bda3450cebfeae90fe9559
Kennedy Mumo <kemumo@microsoft.com>
2022-03-23 11:22:28 -0700

    Make base64 generator synchronous method

--- windows/ReactNativeGetRandomValues/ReactNativeGetRandomValues.h
+++ windows/ReactNativeGetRandomValues/ReactNativeGetRandomValues.h
@@ -7,7 +7,7 @@ namespace winrt::ReactNativeGetRandomValues
     REACT_MODULE(ReactNativeGetRandomValues, L"RNGetRandomValues");
     struct ReactNativeGetRandomValues
     {
-        REACT_METHOD(getRandomBase64)
+        REACT_SYNC_METHOD(getRandomBase64)
         std::string getRandomBase64(int byteLength) noexcept;
     };
 }
